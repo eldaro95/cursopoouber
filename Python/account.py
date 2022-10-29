a79c73e0c09c00ed378706976256a8ee2bd82cd7
Daro <kdhidalgoa@hotmail.com>
2022-10-29 16:47:30 -0500

    Agregado constructores a Python

--- Python/account.py
+++ Python/account.py
@@ -4,3 +4,14 @@ class Account:
     document = str
     email = str
     password = str
+    
+    #Declarando el metodo constructor (init y self son obligatorios)
+    def __init__(self, name, document):
+        self.name = name
+        self.document = document
+        
+    '''hace referencia a los datos que componen la clase,
+    en este caso self.name está llamando al atributo name 
+    que se encuentra en la línea 3 de la clase y, le está
+    asignando el dato que se pasa en el método __init__ de la línea 8.
+'''
--- Python/car.py
+++ Python/car.py
@@ -1,5 +1,16 @@
+#Importar para usar herencia
+from account import Account
+
+
 class Car:
     id = int
     licence = str
-    driver = str
+    driver = Account("","") #Por herencia recibe lo de account
     passenger = int
+    
+    #Declarando el metodo constructor (init y self son obligatorios)
+    def __init__(self, license, driver):
+        self.license = license
+        self.driver = driver
+    
+    #Campos obligatorios
