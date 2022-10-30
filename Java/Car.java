9e2eb77a9e6cf3798c902c1a21e3d865db129f56
Daro <kdhidalgoa@hotmail.com>
2022-10-29 22:45:05 -0500

    Encapsulamiento pasajeros en Java

--- Java/Car.java
+++ Java/Car.java
@@ -2,15 +2,31 @@ class Car {
     Integer id;
     String license;
     Account driver;
-    Integer passenger;
+    private Integer passenger;
 
     public Car(String license, Account driver){
         this.license = license;
         this.driver = driver;
     }
     void printDataCar() {
+        if (passenger != null){
         System.out.println("\nLicence: " + license + "\nName Driver: " + driver.name + "\nPassengers: " + passenger);
+        } else {
+            System.out.println("");
+        }
     }
-    
-    
+
+    /*Forma de poder llamar los datos privados en una clase hija */
+    public Integer getPassenger() {
+        return passenger;
+    }
+    /*Sirver para establecer condiciones de validación de una variable */
+    public void setPassenger(Integer passenger){
+        if(passenger>=4){
+            this.passenger = passenger;
+        } else {
+            System.out.println("Necesitas asignar un numero mayor a 4 de pasajeros");
+        }
+        
+    }
 }
--- Java/Main.java
+++ Java/Main.java
@@ -1,11 +1,13 @@
 class Main{
     public static void main(String[] args) {
         UberX uberx = new UberX("AMQ123",new Account("Pepe","160058431","elpepe@gmail.com"),"Chevrolet","Saint");
-        uberx.passenger = 5;
+        /*uberx.passenger = 5; Se encapsulo en la clase como private*/
+        uberx.setPassenger(3);
         uberx.printDataCar();
 
-        Car car2= new Car("AGG184",new Account("Juan","156606541","juanito@gmail.com"));
-        car2.passenger=3;
+        UberPool car2= new UberPool("AGG184",new Account("Juan","156606541","juanito@gmail.com"),"Audi","V2");
+        /*car2.passenger=3; encapsulado*/
+        car2.setPassenger(4);
         car2.printDataCar();
     }
 }
