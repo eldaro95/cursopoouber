1bdf4561d69bb83c259b300a2d03a79dbee8dc9c
Daro <kdhidalgoa@hotmail.com>
2022-10-29 21:42:52 -0500

    Agregado python con sus herencias, se obvia JS

--- Python/main.py
+++ Python/main.py
@@ -1,7 +1,15 @@
 from car import Car
 from account import Account
+from uberblack import UberBlack
+from uberx import UberX
 
 if __name__ == "__main__" :
     car = Car("ASG1024", Account("Pepe Grillo","187555001"))
     print(vars(car))
     print(vars(car.driver))
+    
+    uberx= UberX("FIUG887",Account("Maria Juarez","1958666944"),"Mercedez","CLA")
+    print(vars(uberx))
+    
+    uberlujo=UberBlack("GII9870",Account("John Salchichon","956858493054"),"Mustang","V1")
+    print(vars(uberlujo))
---
+++ Python/uberblack.py
@@ -0,0 +1,12 @@
+from car import Car
+
+class UberBlack(Car):
+    typeCarAccepted = [];
+    material = [];
+    
+    #Aplicando herencia de Car
+    def __init__(self,license,driver,typeCarAccepted,material):
+        super().__init__(license,driver)
+        self.typeCarAccepted = typeCarAccepted
+        self.material = material
+        
